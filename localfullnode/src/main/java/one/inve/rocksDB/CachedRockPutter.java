1563d0e1516f43642d4803ffbdc2f989fcb7978f
Francis.Deng <1142539508@qq.com>
2019-04-18 01:10:56 -0700

    An outsider is able to customize initial values.

--- localfullnode/src/main/java/one/inve/rocksDB/CachedRockPutter.java
+++ localfullnode/src/main/java/one/inve/rocksDB/CachedRockPutter.java
@@ -20,6 +20,11 @@ public class CachedRockPutter extends SnailRockPutter {
 	private List<String> keys;
 
 	public CachedRockPutter(String dbId, String... keys) {
+		this(dbId, 5L, 1000L, keys);
+	}
+
+	// An outsider is able to customize initial values.
+	public CachedRockPutter(String dbId, long capacity, long intervalInMilli, String... keys) {
 		super(dbId, 5L, 1000L);
 		this.keys = Arrays.asList(keys);
 	}
