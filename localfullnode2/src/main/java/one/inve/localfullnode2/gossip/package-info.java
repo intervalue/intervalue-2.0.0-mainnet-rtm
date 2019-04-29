8886f8fb21ec78e55f59cfef546d3ca6037efc4d
Francis.Deng <1142539508@qq.com>
2019-04-29 01:18:11 -0700

    Gossip plays an important role in peer to peer communication

--- localfullnode2/src/main/java/one/inve/localfullnode2/gossip/package-info.java
+++ localfullnode2/src/main/java/one/inve/localfullnode2/gossip/package-info.java
@@ -4,7 +4,7 @@
  * 
  * @Description: Gossip plays an important role in peer to peer communication
  * @author: Francis.Deng
- * @date: Apr 29, 2019 1:15:42 AM
+ * @date: Apr 29, 2018 1:15:42 AM
  * @version: V1.0
  */
 
