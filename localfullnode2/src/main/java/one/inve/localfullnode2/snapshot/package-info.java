4b8ef6ff95d5c2656b4e4dd02ab7c96295a66e91
Francis.Deng <1142539508@qq.com>
2019-04-29 00:40:38 -0700

    initialize localfullnode2

---
+++ localfullnode2/src/main/java/one/inve/localfullnode2/snapshot/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ * Copyright © CHXX Co.,Ltd. All rights reserved.
+ * 
+ * @Description: TODO
+ * @date: Apr 29, 2019 12:34:13 AM
+ * @version: V1.0
+ */
+package one.inve.localfullnode2.snapshot;
