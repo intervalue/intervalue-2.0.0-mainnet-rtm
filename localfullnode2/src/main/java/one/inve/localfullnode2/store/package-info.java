32acd026574e4bdd666b81e5764592d9694eb690
Francis.Deng <1142539508@qq.com>
2019-04-29 23:05:43 -0700

    more description

--- localfullnode2/src/main/java/one/inve/localfullnode2/store/package-info.java
+++ localfullnode2/src/main/java/one/inve/localfullnode2/store/package-info.java
@@ -1,19 +1,10 @@
-/**  
-
- * Copyright © CHXX Co.,Ltd. All rights reserved.  
- *   
- * @Description: TODO 
- * @author: Francis.Deng 
- * @date: Apr 29, 2019 8:27:29 PM 
- * @version: V1.0  
- */
-/**  
-
- * Copyright © CHXX Co.,Ltd. All rights reserved.  
- *   
- * @Description: TODO 
- * @author: Francis.Deng 
- * @date: Apr 29, 2019 8:27:29 PM 
- * @version: V1.0  
+/**
+ * 
+ * Copyright © CHXX Co.,Ltd. All rights reserved.
+ * 
+ * @Description: provide approaches to bottomed data
+ * @author: Francis.Deng
+ * @date: Apr 29, 2019 8:27:29 PM
+ * @version: V1.0
  */
 package one.inve.localfullnode2.store;
