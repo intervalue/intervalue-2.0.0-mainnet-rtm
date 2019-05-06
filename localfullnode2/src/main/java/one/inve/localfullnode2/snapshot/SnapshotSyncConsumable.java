c38fe9d5a925758486c2edae15a7869be917e37a
Francis.Deng <1142539508@qq.com>
2019-05-06 01:58:15 -0700

    initial snapshot sync

---
+++ localfullnode2/src/main/java/one/inve/localfullnode2/snapshot/SnapshotSyncConsumable.java
@@ -0,0 +1,21 @@
+package one.inve.localfullnode2.snapshot;
+
+import java.util.concurrent.CompletableFuture;
+
+import one.inve.cluster.Member;
+import one.inve.localfullnode2.gossip.vo.GossipObj;
+
+/**
+ * 
+ * Copyright © CHXX Co.,Ltd. All rights reserved.
+ * 
+ * @Description: provide snapshot sync communication utility
+ * @author: Francis.Deng
+ * @date: Dec 6, 2018 1:31:23 AM
+ * @version: V1.0
+ */
+public interface SnapshotSyncConsumable {
+	// snapshot synchronizing
+	CompletableFuture<GossipObj> gossipMySnapVersion4SnapAsync(Member neighbor, String pubkey, String sig, String hash,
+			String transCount);
+}
