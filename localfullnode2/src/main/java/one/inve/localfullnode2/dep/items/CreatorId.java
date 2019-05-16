fa9a8a627657e90cf2e2f71711170d65b23cbd52
Francis.Deng <1142539508@qq.com>
2019-05-15 19:00:02 -0700

    turn creatId into long type

--- localfullnode2/src/main/java/one/inve/localfullnode2/dep/items/CreatorId.java
+++ localfullnode2/src/main/java/one/inve/localfullnode2/dep/items/CreatorId.java
@@ -3,13 +3,13 @@ package one.inve.localfullnode2.dep.items;
 import one.inve.localfullnode2.dep.DependentItem;
 
 public class CreatorId extends DependentItem {
-	private int creatorId;
+	private long creatorId;
 
-	public int get() {
+	public long get() {
 		return creatorId;
 	}
 
-	public void set(int creatorId) {
+	public void set(long creatorId) {
 		this.creatorId = creatorId;
 	}
 
