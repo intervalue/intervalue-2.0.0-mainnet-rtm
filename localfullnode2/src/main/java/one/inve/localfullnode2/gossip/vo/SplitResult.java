c2d31dcce8f83d198ccce6e38dddc813e0999c05
Francis.Deng <1142539508@qq.com>
2019-05-16 01:51:57 -0700

    missing part

---
+++ localfullnode2/src/main/java/one/inve/localfullnode2/gossip/vo/SplitResult.java
@@ -0,0 +1,43 @@
+package one.inve.localfullnode2.gossip.vo;
+
+import java.util.Set;
+
+public class SplitResult {
+
+	/**
+	 * 密钥
+	 * Created by Max on 2018/6/10.
+	 */
+	    public long[] minSeq;
+	    public Set<String> delKey;
+
+	    public SplitResult() {
+	    }
+
+	    private SplitResult(Builder builder) {
+	    	minSeq = builder.minSeq;
+	    	delKey = builder.delKey;
+	    }
+
+	    public static final class Builder {
+	        private long[] minSeq;
+	        private Set<String> delKey;
+
+	        public Builder() {
+	        }
+
+	        public Builder minSeq(long[] val) {
+	        	minSeq = val;
+	            return this;
+	        }
+
+	        public Builder delKey(Set<String> val) {
+	        	delKey = val;
+	            return this;
+	        }
+
+	        public SplitResult build() {
+	            return new SplitResult(this);
+	        }
+	    }
+}
