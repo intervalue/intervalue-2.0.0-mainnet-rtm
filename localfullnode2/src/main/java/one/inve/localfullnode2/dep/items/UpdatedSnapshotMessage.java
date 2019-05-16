ce76cc704991dc6e5dab8bb4eb3b9ddaea9babcb
Francis.Deng <1142539508@qq.com>
2019-05-16 00:29:54 -0700

    UpdatedSnapshotMessage onboard

---
+++ localfullnode2/src/main/java/one/inve/localfullnode2/dep/items/UpdatedSnapshotMessage.java
@@ -0,0 +1,18 @@
+package one.inve.localfullnode2.dep.items;
+
+import one.inve.bean.message.SnapshotMessage;
+import one.inve.localfullnode2.dep.DependentItem;
+
+public class UpdatedSnapshotMessage extends DependentItem {
+	private SnapshotMessage snapshotMessage;
+
+	public void set(SnapshotMessage snapshotMessage) {
+		this.snapshotMessage = snapshotMessage;
+		nodifyAll();
+	}
+
+	public SnapshotMessage get() {
+		return snapshotMessage;
+	}
+
+}
