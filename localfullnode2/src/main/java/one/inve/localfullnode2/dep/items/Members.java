1cc1be1b9cb94f7884bd56f1270a2bdd5a324f63
Francis.Deng <1142539508@qq.com>
2019-05-15 23:11:00 -0700

    by types

--- localfullnode2/src/main/java/one/inve/localfullnode2/dep/items/Members.java
+++ localfullnode2/src/main/java/one/inve/localfullnode2/dep/items/Members.java
@@ -3,17 +3,25 @@ package one.inve.localfullnode2.dep.items;
 import java.util.List;
 
 import one.inve.cluster.Member;
+import one.inve.localfullnode2.conf.Config;
 import one.inve.localfullnode2.dep.DependentItem;
 
 public class Members extends DependentItem {
-	private List<Member> members;
 
-	public List<Member> get() {
-		return members;
+	private List<Member> membersInSharding;
+	private List<Member> membersGlobally;
+
+	public List<Member> get(int inShardingOrGlobally) {
+		return inShardingOrGlobally == Config.GOSSIP_GLOBAL_SHARD ? membersGlobally : membersInSharding;
 	}
 
-	public void set(List<Member> members) {
-		this.members = members;
+	public void setInSharding(List<Member> members) {
+		this.membersInSharding = members;
+		nodifyAll();
+	}
+
+	public void setGlobally(List<Member> members) {
+		this.membersGlobally = members;
 		nodifyAll();
 	}
 
