6d3c0f3397f4694fd920004e956f199da19fd589
Francis.Deng <1142539508@qq.com>
2019-05-28 23:25:16 -0700

    introduce L2LCore

--- localfullnode2/src/main/java/one/inve/localfullnode2/rpc/impl/Local2localImpl.java
+++ localfullnode2/src/main/java/one/inve/localfullnode2/rpc/impl/Local2localImpl.java
@@ -27,6 +27,7 @@ import com.zeroc.Ice.Current;
 import one.inve.bean.node.NodeStatus;
 import one.inve.localfullnode2.conf.Config;
 import one.inve.localfullnode2.gossip.HandleSplitReportThread;
+import one.inve.localfullnode2.gossip.l2l.L2LCore;
 import one.inve.localfullnode2.gossip.vo.AppointEvent;
 import one.inve.localfullnode2.gossip.vo.GossipObj;
 import one.inve.localfullnode2.gossip.vo.SplitResult;
@@ -104,75 +105,91 @@ public class Local2localImpl implements Local2local {
 	 *                    contains a creatorId and a sequenceId.
 	 * @param current     rpc connection info
 	 */
+//	@Override
+//	public synchronized GossipObj gossipMyMaxSeqList4Consensus(String pubkey, String sig, String snapVersion,
+//			String snapHash, long[] seqs, Current current) {
+//		GossipObj gossipObj = null;
+//		if (gossipFlag) {
+//			Instant first = Instant.now();
+//			if (!validate(pubkey)) {
+//				return null;
+//			}
+//
+//			// Francis.Deng 4/4/2019
+//			// There was an accident that x.xx.202.66:35791(who had higher snapVersion
+//			// "273")
+//			// retained lower seqs watermark than other's(who had snapVersion "272"
+//			// meanwhile),
+//			// which caused serious gossip problem.
+//
+//			// Each thing is fine if the gap between requester's snapVersion and
+//			// responder‘snapVersion is 0 or 1
+//			BigInteger currSnapshotVersion = node.getCurrSnapshotVersion();
+//			if (currSnapshotVersion.equals(new BigInteger(snapVersion))
+//					|| currSnapshotVersion.subtract(BigInteger.ONE).equals(new BigInteger(snapVersion))
+//					|| currSnapshotVersion.add(BigInteger.ONE).equals(new BigInteger(snapVersion))) {
+//				// 请求者地址信息
+//				String addressInfo = (null == current.con) ? null : current.con.toString();
+//				logger.info("requestor's pubkey = {}", pubkey);
+//				List<Event> rpcEvents = getUnknownEvents(seqs, addressInfo).stream()
+//						.map(eventBody -> new Event(eventBody.getShardId(), eventBody.getCreatorId(),
+//								eventBody.getCreatorSeq(), eventBody.getOtherId(), eventBody.getOtherSeq(),
+//								eventBody.getTrans(), eventBody.getTimeCreated().getEpochSecond(),
+//								eventBody.getTimeCreated().getNano(), eventBody.getSignature(), eventBody.isFamous(),
+//								eventBody.getHash(), eventBody.getGeneration(),
+//								(null == eventBody.getConsTimestamp()) ? -1
+//										: eventBody.getConsTimestamp().getEpochSecond(),
+//								(null == eventBody.getConsTimestamp()) ? -1 : eventBody.getConsTimestamp().getNano(),
+//								eventBody.getOtherHash(), eventBody.getParentHash()))
+//						.collect(Collectors.toList());
+//
+//				gossipObj = (rpcEvents.size() > 0)
+//						? new GossipObj(currSnapshotVersion.toString(), rpcEvents.toArray(new Event[0]), null)
+//						: new GossipObj(currSnapshotVersion.toString(), null, null);
+//
+//				long handleInterval = Duration.between(first, Instant.now()).toMillis();
+//				if (handleInterval > Config.DEFAULT_GOSSIP_EVENT_INTERVAL) {
+//					logger.warn(
+//							"Warning:gossipMyMaxSeqList4Consensus in server-side costs {} ms.  Additional connection(remote address-->local address) is <<{}>>",
+//							handleInterval, addressInfo);
+//				}
+//				return gossipObj;
+//
+//			} else if (currSnapshotVersion.compareTo(new BigInteger(snapVersion)) > 0) {
+//				/*
+//				 * logger.warn("hash:{}",SnapshotDbService.querySnapshotMessageHashByVersion(
+//				 * node.nodeParameters.dbId,snapVersion)==null?
+//				 * null:SnapshotDbService.querySnapshotMessageHashByVersion(node.nodeParameters.
+//				 * dbId,snapVersion));
+//				 * logger.warn("{}",SnapshotDbService.querySnapshotMessageHashByVersion(node.
+//				 * nodeParameters.dbId,snapVersion)==null?
+//				 * null:SnapshotDbService.querySnapshotMessageHashByVersion(node.nodeParameters.
+//				 * dbId,snapVersion).getBytes());
+//				 */
+//				return new GossipObj(currSnapshotVersion.toString(), null,
+//						snapshotDbService.querySnapshotMessageHashByVersion(node.nodeParameters().dbId,
+//								snapVersion) == null ? null
+//										: snapshotDbService.querySnapshotMessageHashByVersion(
+//												node.nodeParameters().dbId, snapVersion).getBytes());
+//			} else {
+//				return new GossipObj(currSnapshotVersion.toString(), null, null);
+//			}
+//		} else { // gossipFlag is false
+//			return gossipObj;
+//		}
+//	}
+	/**
+	 * delegate core task to {@link L2LCore}
+	 */
 	@Override
 	public synchronized GossipObj gossipMyMaxSeqList4Consensus(String pubkey, String sig, String snapVersion,
 			String snapHash, long[] seqs, Current current) {
 		GossipObj gossipObj = null;
 		if (gossipFlag) {
-			Instant first = Instant.now();
-			if (!validate(pubkey)) {
-				return null;
-			}
-
-			// Francis.Deng 4/4/2019
-			// There was an accident that x.xx.202.66:35791(who had higher snapVersion
-			// "273")
-			// retained lower seqs watermark than other's(who had snapVersion "272"
-			// meanwhile),
-			// which caused serious gossip problem.
-
-			// Each thing is fine if the gap between requester's snapVersion and
-			// responder‘snapVersion is 0 or 1
-			BigInteger currSnapshotVersion = node.getCurrSnapshotVersion();
-			if (currSnapshotVersion.equals(new BigInteger(snapVersion))
-					|| currSnapshotVersion.subtract(BigInteger.ONE).equals(new BigInteger(snapVersion))
-					|| currSnapshotVersion.add(BigInteger.ONE).equals(new BigInteger(snapVersion))) {
-				// 请求者地址信息
-				String addressInfo = (null == current.con) ? null : current.con.toString();
-				logger.info("requestor's pubkey = {}", pubkey);
-				List<Event> rpcEvents = getUnknownEvents(seqs, addressInfo).stream()
-						.map(eventBody -> new Event(eventBody.getShardId(), eventBody.getCreatorId(),
-								eventBody.getCreatorSeq(), eventBody.getOtherId(), eventBody.getOtherSeq(),
-								eventBody.getTrans(), eventBody.getTimeCreated().getEpochSecond(),
-								eventBody.getTimeCreated().getNano(), eventBody.getSignature(), eventBody.isFamous(),
-								eventBody.getHash(), eventBody.getGeneration(),
-								(null == eventBody.getConsTimestamp()) ? -1
-										: eventBody.getConsTimestamp().getEpochSecond(),
-								(null == eventBody.getConsTimestamp()) ? -1 : eventBody.getConsTimestamp().getNano(),
-								eventBody.getOtherHash(), eventBody.getParentHash()))
-						.collect(Collectors.toList());
-
-				gossipObj = (rpcEvents.size() > 0)
-						? new GossipObj(currSnapshotVersion.toString(), rpcEvents.toArray(new Event[0]), null)
-						: new GossipObj(currSnapshotVersion.toString(), null, null);
-
-				long handleInterval = Duration.between(first, Instant.now()).toMillis();
-				if (handleInterval > Config.DEFAULT_GOSSIP_EVENT_INTERVAL) {
-					logger.warn(
-							"Warning:gossipMyMaxSeqList4Consensus in server-side costs {} ms.  Additional connection(remote address-->local address) is <<{}>>",
-							handleInterval, addressInfo);
-				}
-				return gossipObj;
-
-			} else if (currSnapshotVersion.compareTo(new BigInteger(snapVersion)) > 0) {
-				/*
-				 * logger.warn("hash:{}",SnapshotDbService.querySnapshotMessageHashByVersion(
-				 * node.nodeParameters.dbId,snapVersion)==null?
-				 * null:SnapshotDbService.querySnapshotMessageHashByVersion(node.nodeParameters.
-				 * dbId,snapVersion));
-				 * logger.warn("{}",SnapshotDbService.querySnapshotMessageHashByVersion(node.
-				 * nodeParameters.dbId,snapVersion)==null?
-				 * null:SnapshotDbService.querySnapshotMessageHashByVersion(node.nodeParameters.
-				 * dbId,snapVersion).getBytes());
-				 */
-				return new GossipObj(currSnapshotVersion.toString(), null,
-						snapshotDbService.querySnapshotMessageHashByVersion(node.nodeParameters().dbId,
-								snapVersion) == null ? null
-										: snapshotDbService.querySnapshotMessageHashByVersion(
-												node.nodeParameters().dbId, snapVersion).getBytes());
-			} else {
-				return new GossipObj(currSnapshotVersion.toString(), null, null);
-			}
+			L2LCore l2l = new L2LCore();
+			return l2l.gossipMyMaxSeqList4Consensus(pubkey, sig, snapVersion, snapHash, seqs,
+					node.getCurrSnapshotVersion(), node.getLocalFullNodes(), node.getEventStore(), node.getShardId(),
+					node.nodeParameters().dbId);
 		} else { // gossipFlag is false
 			return gossipObj;
 		}
