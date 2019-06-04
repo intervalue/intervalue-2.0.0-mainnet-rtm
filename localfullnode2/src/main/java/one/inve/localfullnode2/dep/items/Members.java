40889f59fff76d18d6412b3488ca5669ec422b56
Francis.Deng <1142539508@qq.com>
2019-06-04 04:07:59 -0700

    introduce thread-safe list

--- localfullnode2/src/main/java/one/inve/localfullnode2/dep/items/Members.java
+++ localfullnode2/src/main/java/one/inve/localfullnode2/dep/items/Members.java
@@ -1,5 +1,6 @@
 package one.inve.localfullnode2.dep.items;
 
+import java.util.Collections;
 import java.util.List;
 
 import one.inve.cluster.Member;
@@ -15,13 +16,17 @@ public class Members extends DependentItem {
 		return inShardingOrGlobally == Config.GOSSIP_GLOBAL_SHARD ? membersGlobally : membersInSharding;
 	}
 
+	/**
+	 * introduce thread-safe list
+	 */
+
 	public void setInSharding(List<Member> members) {
-		this.membersInSharding = members;
+		this.membersInSharding = Collections.synchronizedList(members);
 		nodifyAll();
 	}
 
 	public void setGlobally(List<Member> members) {
-		this.membersGlobally = members;
+		this.membersGlobally = Collections.synchronizedList(members);
 		nodifyAll();
 	}
 
