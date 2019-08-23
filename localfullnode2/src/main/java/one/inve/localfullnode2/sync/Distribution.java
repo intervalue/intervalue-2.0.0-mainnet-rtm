0c5f33f66cb9e9a79fe76c63b3970b58cd15cab4
francis <francis@ubuntu>
2019-08-23 01:06:11 -0700

    Distribution constitutes 1 or a couple of {@code Column}

--- localfullnode2/src/main/java/one/inve/localfullnode2/sync/Distribution.java
+++ localfullnode2/src/main/java/one/inve/localfullnode2/sync/Distribution.java
@@ -3,9 +3,25 @@ package one.inve.localfullnode2.sync;
 import one.inve.localfullnode2.sync.criteria.Range;
 import one.inve.localfullnode2.utilities.GenericArray;
 
+/**
+ * 
+ * Copyright © INVE FOUNDATION. All rights reserved.
+ * 
+ * @ClassName: Distribution
+ * @Description: Distribution constitutes 1 or a couple of {@code Column},the
+ *               former is message distribution
+ * @author Francis.Deng
+ * @mailbox francis_xiiiv@163.com
+ * @date Aug 23, 2019
+ *
+ */
 public class Distribution {
 	public static class Column {
 		private GenericArray<Range> ranges = new GenericArray<>();
+
+		public GenericArray<Range> getRanges() {
+			return ranges;
+		}
 
 		public void add(Range r) {
 			for (Range range : ranges) {
@@ -16,9 +32,19 @@ public class Distribution {
 
 			ranges.append(r);
 		}
+
+		public void add(Column column) {
+			for (Range range : column.getRanges()) {
+				add(range);
+			}
+		}
 	}
 
 	private Column[] columns;
+
+	public Column[] getColumns() {
+		return columns;
+	}
 
 	public Distribution(int columnNum) {
 		columns = new Column[columnNum];
@@ -27,6 +53,13 @@ public class Distribution {
 
 	public void addLabeledRange(int label, Range r) {
 		columns[label].add(r);
+	}
+
+	// merge another distribution
+	public void addDistribution(Distribution dist) {
+		for (int index = 0; index < dist.getColumns().length; index++) {
+			columns[index].add(dist.getColumns()[index]);
+		}
 	}
 
 }
