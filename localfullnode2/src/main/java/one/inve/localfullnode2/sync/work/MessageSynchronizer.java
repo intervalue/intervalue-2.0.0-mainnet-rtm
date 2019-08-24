6e1ae56776a2cacf0536643528113e74f640d408
francis <francis@ubuntu>
2019-08-24 02:23:59 -0700

    core sync work

--- localfullnode2/src/main/java/one/inve/localfullnode2/sync/work/MessageSynchronizer.java
+++ localfullnode2/src/main/java/one/inve/localfullnode2/sync/work/MessageSynchronizer.java
@@ -3,10 +3,13 @@ package one.inve.localfullnode2.sync.work;
 import java.math.BigInteger;
 import java.util.concurrent.BlockingQueue;
 
+import com.alibaba.fastjson.JSONObject;
+
 import one.inve.core.EventBody;
-import one.inve.localfullnode2.gossip.persistence.NewGossipEventsPersistence;
-import one.inve.localfullnode2.gossip.persistence.NewGossipEventsPersistenceDependent;
+import one.inve.localfullnode2.message.MessagePersistence;
+import one.inve.localfullnode2.message.MessagePersistenceDependent;
 import one.inve.localfullnode2.staging.StagingArea;
+import one.inve.localfullnode2.store.rocks.INosql;
 import one.inve.localfullnode2.sync.DistributedO;
 import one.inve.localfullnode2.sync.Distribution;
 import one.inve.localfullnode2.sync.IContext;
@@ -18,13 +21,13 @@ import one.inve.localfullnode2.sync.SynchronizationWork.BasedIterativePart;
  * 
  * Copyright © INVE FOUNDATION. All rights reserved.
  * 
- * @ClassName: EventSynchronizer
- * @Description: event synchronizer
+ * @ClassName: MessageSynchronizer
+ * @Description: message synchronizer
  * @author Francis.Deng [francis_xiiiv@163.com]
  * @date Aug 24, 2019
  *
  */
-public class EventSynchronizer extends BasedIterativePart {
+public class MessageSynchronizer extends BasedIterativePart {
 
 	@Override
 	public void runOnce(IContext context) {
@@ -33,28 +36,37 @@ public class EventSynchronizer extends BasedIterativePart {
 
 		ISyncSourceProfile srcProfile = getSourceProfile(context);
 
-		DistributedO<EventBody> distributedObjects = synSource.getNotInDistributionEvents(myDist);
+		DistributedO<JSONObject> distributedObjects = synSource.getNotInDistributionMessages(myDist);
 		if (distributedObjects.getObjects() == null || distributedObjects.getObjects().length == 0) {
 			done = true;
 			return;
 		}
 		// int length = distributedObjects.getObjects().length;
 
-		// handle the batch of EventBodies
-		NewGossipEventsPersistence newGossipEventsPersistence = new NewGossipEventsPersistence();
-		newGossipEventsPersistence.persistNewEvents(new NewGossipEventsPersistenceDependent() {
+		// handle the batch of Messges
+		MessagePersistence messagePersistence = new MessagePersistence(new MessagePersistenceDependent() {
 
 			@Override
-			public BlockingQueue<EventBody> getEventSaveQueue() {
+			public BlockingQueue<JSONObject> getConsMessageSaveQueue() {
 				StagingArea stagingArea = new StagingArea();
-				stagingArea.createQueue(EventBody.class, StagingArea.EventSaveQueueName, 10000000, null);
+				stagingArea.createQueue(EventBody.class, StagingArea.ConsMessageSaveQueueName, 10000000, null);
 
-				BlockingQueue<EventBody> q = stagingArea.getQueue(EventBody.class, StagingArea.EventSaveQueueName);
-				for (EventBody eb : distributedObjects.getObjects()) {
-					q.add(eb);
+				BlockingQueue<JSONObject> q = stagingArea.getQueue(JSONObject.class, StagingArea.EventSaveQueueName);
+				for (JSONObject jsonizedMessage : distributedObjects.getObjects()) {
+					q.add(jsonizedMessage);
 				}
 
 				return q;
+			}
+
+			@Override
+			public void setConsMessageCount(BigInteger consMessageCount) {
+				return;
+			}
+
+			@Override
+			public BigInteger getConsMessageCount() {
+				return BigInteger.ZERO;
 			}
 
 			@Override
@@ -62,18 +74,19 @@ public class EventSynchronizer extends BasedIterativePart {
 				return srcProfile.getDBId();
 			}
 
-			// ignore {@code addTotalEventCount} and {@code getTotalEventCount}
 			@Override
-			public void addTotalEventCount(long delta) {
-				return;
+			public INosql getNosql() {
+				// TODO Auto-generated method stub
+				return null;
 			}
 
 			@Override
-			public BigInteger getTotalEventCount() {
-				return BigInteger.ZERO;
+			public BlockingQueue<JSONObject> getSystemAutoTxSaveQueue() {
+				return null;
 			}
 
 		});
+		messagePersistence.persisMessages();
 
 		context.joinDistribution(distributedObjects.getDist());
 
---
+++ localfullnode2/src/main/java/one/inve/localfullnode2/sync/work/SystemMessageSynchronizer.java
@@ -0,0 +1,20 @@
+package one.inve.localfullnode2.sync.work;
+
+import one.inve.localfullnode2.sync.IContext;
+import one.inve.localfullnode2.sync.SynchronizationWork.BasedIterativePart;
+
+public class SystemMessageSynchronizer extends BasedIterativePart {
+
+	@Override
+	public boolean isDone() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void runOnce(IContext context) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
