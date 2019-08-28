14a69be7893f90713f92361d0140090efc8a6ee1
francis <francis@ubuntu>
2019-08-28 00:24:19 -0700

    rocksdb key envelope

---
+++ localfullnode2/src/main/java/one/inve/localfullnode2/store/rocks/key/RocksdbKey.java
@@ -0,0 +1,15 @@
+package one.inve.localfullnode2.store.rocks.key;
+
+/**
+ * 
+ * Copyright © INVE FOUNDATION. All rights reserved.
+ * 
+ * @ClassName: RocksdbKey
+ * @Description: TODO
+ * @author Francis.Deng [francis_xiiiv@163.com]
+ * @date Aug 27, 2019
+ *
+ */
+public interface RocksdbKey {
+	byte[] toBytes();
+}
