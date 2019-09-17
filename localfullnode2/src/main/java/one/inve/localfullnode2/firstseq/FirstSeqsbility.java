8188ff1161cf4a8a8eca2dca365e2f735bf56afd
Francis.Deng <1142539508@qq.com>
2019-09-17 00:59:21 -0700

    cause stackOverflowError

--- localfullnode2/src/main/java/one/inve/localfullnode2/firstseq/FirstSeqsbility.java
+++ localfullnode2/src/main/java/one/inve/localfullnode2/firstseq/FirstSeqsbility.java
@@ -60,7 +60,7 @@ public class FirstSeqsbility {
 	// key function to find the bottom of event sequence
 	protected BigInteger binarySearch(int shardId, int idInShard, BigInteger fromSeq, BigInteger toSeq,
 			IEventStoreBility eventStore) {
-
+		// cause stackOverflowError
 		// logger.info("binary search first seq: [{},{}]", fromSeq.longValue(),
 		// toSeq.longValue());
 
