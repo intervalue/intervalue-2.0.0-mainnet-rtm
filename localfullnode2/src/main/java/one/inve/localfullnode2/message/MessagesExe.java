71dfda93620c4da878557ef20c92e28191977193
Francis.Deng <1142539508@qq.com>
2019-11-17 19:41:59 -0800

    with the result of "not enough balance in the address(??)"

--- localfullnode2/src/main/java/one/inve/localfullnode2/message/MessagesExe.java
+++ localfullnode2/src/main/java/one/inve/localfullnode2/message/MessagesExe.java
@@ -218,8 +218,8 @@ public class MessagesExe {
 			} else {
 				valid = verifyDoubleCost(fromAddress, toAddress, fee, tm.getAmount());
 				if (!valid) {
-					// Francis.Deng with the result of "not enough balance"
-					msgObject.put("error", "not enough balance");
+					// Francis.Deng with the result of "not enough balance in the address(??)"
+					msgObject.put("error", "not enough balance in the address(" + fromAddress + ")");
 
 //					logger.error("node-({}, {}): Transaction message double cost. msgObj: {}", node.getShardId(),
 //							node.getCreatorId(), msgObject.toJSONString());
